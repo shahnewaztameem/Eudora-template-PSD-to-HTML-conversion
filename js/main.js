5a974cbe6385d74081ae7084beee811d232411da
Shahnewaz Tameem <shahnewaztamim@gmail.com>
2020-02-14 00:58:34 +0600

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(window).scroll(function(){
+    var top = $(window).scrollTop();
+    if(top>=112) {
+        $("header").addClass('color-scroll');
+    }
+    else{
+        if($("header").hasClass('color-scroll')){
+            $("header").removeClass('color-scroll')
+        }
+    }
+});
+
